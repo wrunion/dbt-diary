ff8dc0f452145844477cbc8fccb7ae3dba50e363
wrunion <winterrunion@gmail.com>
2021-06-25 18:13:35 -0700

    Add async handlers and configure db connection.

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,5 +1,44 @@
 const db = require('../db')
-// app.get('/:id', (req, res, next) => {
+
+const trainers = [
+  { id: '1', name: 'Winter', favoritePokemon: 'pikachu', pokemon: ['squirtle', 'charizard', 'pikachu'] },
+  { id: '2', name: 'Elle', favoritePokemon: 'scyther', pokemon: ['bulbasaur', 'scorbunny', 'pikachu', 'umbreon', 'scyther'] },
+  { id: '3', name: 'Isnardo', favoritePokemon: 'bulbasaur',pokemon: ['squirtle', 'charizard', 'bulbasaur'] }
+]
+
+module.exports = (router) => {
+
+  router.get('/pokemon', async (req, res) => {
+    res.send('<h1>Pokemon</h1>')
+  })
+
+  router.get('/pokemon/upload', async (req, res) => {
+    res.send('<form action="/pokemon/upload" method="POST"><input type="file"><button type="submit">Upload</button></form>')
+  })
+
+  router.post('/pokemon/upload', async (req, res) => {
+    // if (req) { console.log(req) }
+  })
+
+  router.get('/pokemon/trainers/elle', async (req, res) => {
+    res.send('You are on the add  pokemon page')
+  })
+
+  router.get('/pokemon/trainers/winter', async (req, res) => {
+    res.send('You are on the add  pokemon page')
+  })
+
+  router.get('/pokemon/trainers/isnardo', async (req, res) => {
+    res.send('You are on the add  pokemon page')
+  })
+
+  router.post('/pokemon/add', async (req, res) => {
+
+  })
+
+}
+
+// router.get('/:id', (req, res, next) => {
 //   db.query('SELECT * FROM users WHERE id = $1', [req.params.id], (err, res) => {
 //     if (err) {
 //       return next(err)
@@ -7,28 +46,3 @@ const db = require('../db')
 //     res.send(res.rows[0])
 //   })
 // })
-
-module.exports = (app) => {
-
-  // Dummy auth function with no logic yet
-  // const isAuth = () => next()
-
-  const trainers = [
-    { id: '1', name: 'Winter', pokemon: ['squirtle', 'charizard', 'pikachu'] },
-    { id: '2', name: 'Elle', pokemon: ['bulbasaur', 'scorbunny', 'pikachu', 'umbreon', 'scyther'] },
-    { id: '3', name: 'Isnardo', pokemon: ['squirtle', 'charizard', 'bulbasaur'] }
-  ]
-
-  app.get('/pokemon', async (req, res) => {
-    res.send('You are on the pokemon page')
-  })
-
-  app.get('/pokemon/add', async (req, res) => {
-    res.send('You are on the add  pokemon page')
-  })
-
-  app.post('/pokemon/add', async (req, res) => {
-    
-  })
-
-}
