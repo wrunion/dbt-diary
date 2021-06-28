d94a110df3931e688bbf79cec052ea56b1579b27
wrunion <winterrunion@gmail.com>
2021-06-28 11:51:31 -0700

    Passport configured and working.

--- config/passport.js
+++ config/passport.js
@@ -2,14 +2,14 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const db = require('./../db')
 const bcrypt = require('bcrypt')
-const { getUser } = require('./../utils/authUtils')
+// const { getUser } = require('./../utils/authUtils')
 
 const isMatch = async (password, hashedPassword) => {
   const match = await bcrypt.compare(password, hashedPassword);
   return (match);
 }
 
-const getUser = async email => {
+const getUserByEmail = async email => {
   const res = await db.query('SELECT * FROM development_user WHERE email = $1', [email]);
   // I can't use es6 in my version of Node
   /* If no user, return false */
@@ -18,10 +18,28 @@ const getUser = async email => {
   return(res.rows[0]);
 }
 
+const getUserById = async id => {
+  const res = await db.query('SELECT * FROM development_user WHERE id = $1', [id]);
+  // I can't use es6 in my version of Node
+  /* If no user, return false */
+  if (!res || !res.rows || res.rows.length < 1) { return false; }
+  /* If user, return user */
+  return(res.rows[0]);
+}
+
+/*
+  * Custom callback function that Passport calls
+  * on initialization
+  * Params: email and password 
+  * It can return a user object, an error message, or "false"
+  * if there is no user.
+  * By convention, Passport's "next" function is called "done"
+*/
+
   const VerifyCallback = async (email, password, next) => {
     try {     
 
-      const user = await getUser(email);
+      const user = await getUserByEmail(email);
 
       if (user) { 
         const isValid = await isMatch(password, user.password);
@@ -39,15 +57,26 @@ const getUser = async email => {
     }
   }
 
-/* Passport defaults to "username" for auth
-*  This tells it we're using email as our unique user ID
+/* 
+* Passport defaults to "username" for user ID
+* This tells it we're using email instead
+* We also pass our custom auth function here
 */
 const strategy = new LocalStrategy({
-  usernameField: email
-});
+    usernameField: 'email'
+  }, 
+  VerifyCallback
+);
 
-passport.use(strategy, VerifyCallback)
+passport.serializeUser((user, done) => done(null, user.id));
 
+passport.deserializeUser(async (userId, done) => {
+  const sessionUser = await getUserById(userId);
+  
+  if (err) { return done(err) }
+  
+  return done(null, sessionUser);
+  }
+)
 
-
-passport.authenticate('local')
+passport.use(strategy)
