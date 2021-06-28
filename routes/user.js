5012914a270cdd691895ed9c1066c479a9672b2d
wrunion <winterrunion@gmail.com>
2021-06-28 14:34:02 -0700

    Clean up code.

--- routes/user.js
+++ routes/user.js
@@ -4,41 +4,18 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 require('dotenv').config()
-
-/* Code related to user authentication */
-
-// TODO: move helper functions to utils
-/* Helper functions for auth */
-const checkUser = async email => {
-  const res = await db.query('SELECT * FROM development_user WHERE email = $1', [email]);
-  return(res.rows.length > 0);
-}
+const auth = require('./../utils/authUtils')
 
 /* 
-  function to get the user
-  the pg response object returns the data in the "rows" array object
-  in this case, there should only be one row
+ * Routes & middleware 
+ * related to user authentication 
+ * including login, logout, adding user, password change
 */
-const getUser = async email => {
-  const res = await db.query('SELECT * FROM development_user WHERE email = $1', [email]);
-
-  /* If no user, return false */
-  if (!res || !res.rows || res.rows.length < 1) { return false; }
-
-  /* If user, return user */
-  return(res.rows[0]);
-}
-
-const isValid = async (password, user) => {
-  const isMatch = await bcrypt.compare(password, user.password);
-  return (isMatch);
-}
 
 module.exports = (app) => {
 
   /* Passport config */
   require('./../config/passport')
-  /* Express Sessions */
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
@@ -48,8 +25,7 @@ module.exports = (app) => {
   );
   app.use(passport.initialize());
   app.use(passport.session());
-  // Allows us to show error msg to user 
-  app.use(flash());
+  app.use(flash()); // Allows us to show error msg to user 
   app.use(function (req, res, next) {
     res.locals.error = req.error || '';
     res.locals.success = req.message || '';
@@ -65,27 +41,18 @@ module.exports = (app) => {
     })
   );
 
-  // const isUser = db.query('SELECT name FROM production_user WHERE email = $1', [req.body.email]);
-
-  const isMatch = async (password, hashedPassword) => {
-    const match = await bcrypt.compare(password, hashedPassword);
-    return (match);
-  }
-
   app.post('/user/add', async (req, res) => {
     try {
     const { name, role, email, password } = req.body;
-    // First, check if email is already registered 
-    // Emails must be unique in our system
-    const user = await checkUser(email);
+    
+    const user = await auth.isUser(email); // Emails must be unique in our system
     if (user) { 
       res.render('user.ejs', { activeTab: 'home', 
       message: 'Email is already registered' 
     })
       return;
     }
-    // 10 is the number of salt rounds
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 salt rounds
     const userRes = await db.query('INSERT INTO development_user (name, role, email, password) VALUES ($1, $2, $3, $4)', [name, role, email, hashedPassword]);
 
     userRes && res.render('user.ejs', { activeTab: 'home', message: 'User registered!' })
@@ -96,18 +63,8 @@ module.exports = (app) => {
     }
   )
 
-  // this can be rewritten once sessions are in place
-  const checkPassword = async (pass) => {
-    const res = await db.query('SELECT password FROM development_user WHERE email = $1', [req.user.email]);
-    if (res && res.rows.length > 0) {
-      const hashedPass = res.rows[0];
-      const isMatch = await bcrypt.compare(pass, hashedPass);
-      return (isMatch);
-    }
-    return false;
-  }
-
   /* Change password */
+  // TODO: finish once session logic is in place
   app.post('/user/password', async (req, res) => {
     try {
       const { oldPassword, newPassword, confirmNewPassword } = req.body;
@@ -117,19 +74,15 @@ module.exports = (app) => {
           message: 'New passwords must match', activeTab: 'settings' 
         })
       }
-
       // Validate old password against db w/helper function
       if (checkPassword(oldPassword)) {
         // upsert the new pass into db 
-
         // On success
         res.render('settings.ejs', { 
           message: 'Password updated!', 
           activeTab: 'settings'
         })
       }
-
-
     } catch (err) {
       next(err)
     }
@@ -143,6 +96,7 @@ module.exports = (app) => {
     });
   });
 
+  // TODO: get these working again
   /* Passport middleware function to protect routes */
   // function isNotAuth(req, res, next) {
   //   if (req.isAuthenticated()) {
@@ -152,21 +106,21 @@ module.exports = (app) => {
   // }
 
   // Protects login page from users already logged in
-  const isNotAuth = (req, res, next) => {
-    req.isAuthenticated ? res.redirect('/home') : next();
-  }
+  // const isNotAuth = (req, res, next) => {
+  //   req.isAuthenticated ? res.redirect('/home') : next();
+  // }
 
-  // Protects routes
-  const isAuth = (req, res, next) => {
-    req.isAuthenticated ? next() : res.redirect('/login');
-  }
+  // // Protects routes
+  // const isAuth = (req, res, next) => {
+  //   req.isAuthenticated ? next() : res.redirect('/login');
+  // }
 
-  // Protects admin-only routes
-  const isAdmin = (req, res, next) => {
-    (req.isAuthenticated && req.user.role === 'admin') 
-    ? next() 
-    : res.redirect('/login');
-  }
+  // // Protects admin-only routes
+  // const isAdmin = (req, res, next) => {
+  //   (req.isAuthenticated && req.user.role === 'admin') 
+  //   ? next() 
+  //   : res.redirect('/login');
+  // }
 
   /* Passport middleware function to protect routes */
   // function isAuth(req, res, next) {
@@ -183,30 +137,4 @@ module.exports = (app) => {
   //   }
   //   res.redirect('/home');
   // }
-
 }
-
-
-
-  // app.post('/login', async (req, res) => {
-  //   try {
-  //     const { email, password } = req.body;
-  //     // Method either returns the user object from postgres, 
-  //     // or "false" if the email isn't registered
-  //     const user = await getUser(email);
-  //     /* Tell passport there is no error (null) but no user (false) */
-  //     if (!user) { return done(null, false, { 
-  //       message: 'Incorrect email address' 
-  //     }) }
-  //     /* Compare passwords using bcrypt */
-  //     const correctPassword = isValid(user, password);
-  //     /* User is valid. Pass the user object to Passport */
-  //     if (correctPassword) { return(done, user) }
-
-  //     return(done, false, { 
-  //       message: 'Password is incorrect'
-  //     });
-  //   } catch (err) {
-  //     return done(err);
-  //   }
-  // })
--- utils/authUtils.js
+++ utils/authUtils.js
@@ -8,19 +8,34 @@ module.exports = {
     const match = await bcrypt.compare(password, hashedPassword);
     return (match);
   },
+  /* Returns false or user */
   getUserByEmail: async email => {
     const res = await db.query('SELECT * FROM development_user WHERE email = $1', [email]);
     // I can't use es6 in my configuration of Node
     if (!res || !res.rows || res.rows.length < 1) { return false; }
     return(res.rows[0]);
   },
+  /* Returns false or user */
   getUserById: async id => {
     const res = await db.query('SELECT * FROM development_user WHERE id = $1', [id]);
     // I can't use es6 in my configuration of Node
     if (!res || !res.rows || res.rows.length < 1) { return false; }
     return(res.rows[0]);
+  },
+  /* Returns true or false */
+  isUser: async email => {
+    const res = await db.query('SELECT * FROM development_user WHERE email = $1', [email]);
+    return(res.rows.length > 0);
+  },
+  // this can be rewritten once sessions are in place
+  checkPassword: async (pass) => {
+    const res = await db.query('SELECT password FROM development_user WHERE email = $1', [req.user.email]);
+    if (res && res.rows.length > 0) {
+      const hashedPass = res.rows[0];
+      const isMatch = await bcrypt.compare(pass, hashedPass);
+      return (isMatch);
+    }
+    return false;
   }
-  
-
 }
 
