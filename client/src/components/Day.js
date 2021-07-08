de17ec65849622fde269c028a3f375af7c47198c
wrunion <winterrunion@gmail.com>
2021-07-07 21:59:14 -0700

    Ratings Form complete

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Segment } from 'semantic-ui-react'
 import Page from './../reusable/Page'
-import Form from './../reusable/Form'
+import Form from './RatingsForm'
 
 const Day = () => {
+
   return(
     <Page title='Daily DBT' subtitle='Rate your day from 0 to 5'>
+      
       <Segment>
         <Form />
       </Segment>
--- client/src/components/Week.js
+++ client/src/components/Week.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import Page from './../reusable/Page'
-import FormikExample from './../reusable/FormikExample'
 
 const Week = () => {
   return(
     <Page title='Week' subtitle="In progress">
-      <FormikExample />
-
     </Page>
   )
 }
