11692ca17f3059d9921db68fb43278606f2e1bee
wrunion <winterrunion@gmail.com>
2021-07-08 14:58:16 -0700

    Begin backend refactor to  Mongodb

---
+++ routes/movie-router.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const MovieCtrl = require('../controllers/movie-ctrl')
+
+const router = express.Router()
+
+router.post('/movie', MovieCtrl.createMovie)
+router.put('/movie/:id', MovieCtrl.updateMovie)
+router.delete('/movie/:id', MovieCtrl.deleteMovie)
+router.get('/movie/:id', MovieCtrl.getMovieById)
+router.get('/movies', MovieCtrl.getMovies)
+router.get('/test', (req, res) => {
+  res.send('<h1>Hello world!</h1>')
+})
+
+module.exports = router
