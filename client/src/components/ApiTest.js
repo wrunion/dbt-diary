689a9c091f9173092c27e300ea4c0b2d2f8e1d1e
wrunion <winterrunion@gmail.com>
2021-07-08 17:44:23 -0700

    Save

---
+++ client/src/components/ApiTest.js
@@ -0,0 +1,39 @@
+import React from 'react'
+// import * as api from './../api'
+
+export default class MovieList extends React.Component {
+  state ={
+    movies: []
+  }
+
+  fetchAll = async () => {
+    const movies = await fetch('api/movies');
+    const data = await movies.json()
+    console.log(data)
+  }
+
+  render() {
+    const movies = this.state;
+    this.fetchAll()
+
+    if (movies) {
+    return(
+      <div>
+        Movies here
+      </div>
+    ) 
+  } else { return null }
+}
+}
+
+
+// const ApiTest = () => {
+//   console.log(api)
+//   return(
+//     <div>
+//       ApiTest
+//     </div>
+//   )
+// }
+
+// export default ApiTest;
