7adde95773a17859daf160d183abd82a86639518
wrunion <winterrunion@gmail.com>
2021-07-15 20:25:24 -0700

    Build out form styles.

--- client/src/components/reusable/Page.js
+++ client/src/components/reusable/Page.js
@@ -3,19 +3,23 @@ import { Header, Icon } from 'semantic-ui-react'
 import './Page.css'
 const { Subheader, Content } = Header
 
-const Page = ({ title, subtitle, children, color, icon }) => {
+const Page = ({ title, subtitle, children, color, icon, textAlign }) => {
 
   return(
     <div className="Page">
-      <Header as='h2' color={color || 'blue'} className='display-title'>
-        {icon && <Icon name={icon} />}
-        <Content>
-          {title}
-        <Subheader>
-          {subtitle}
-        </Subheader>
-        </Content>
-      </Header>
+      <div id='page-header'>
+        <Header as='h2' color={color || 'blue'}       
+          className='display-title' textAlign={textAlign || 'center'}>
+          {icon && <Icon name={icon} />}
+          <Content>
+            {title}
+          <Subheader>
+            {subtitle}
+          </Subheader>
+          </Content>
+        </Header>
+      </div>
+
       {children}
     </div>
   )
