513409bec09a7537b502d2de0466d90da49e1e75
wrunion <winterrunion@gmail.com>
2021-07-19 16:42:56 -0700

    Reusable Form component complete

--- client/src/components/Week/WeekInReviewform.js
+++ client/src/components/Week/WeekInReviewform.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect, createRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Segment, Form, Input, Label, Header, Button, Dimmer, Icon } from 'semantic-ui-react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 const { Field } = Form
 const { Dimmable } = Dimmer
 const { Subheader } = Header
 
 const WeekInReview = () => {
 
-  // form input props
+  const [success, setSuccess] = useState(false)
+
   const inputs = [
     { name: 'weekNumber', label: 'Week #', type:'number' },
     { name: 'focus', label: 'Focus this week', type:'text'},
@@ -17,13 +18,36 @@ const WeekInReview = () => {
 
   const onSubmitCallback = (data) => {
     console.log('onSubmitCallback', data)
+    setSuccess(true)
+    // submit to server
+    // fetch('api/week/create', {
+    //   method: 'POST', 
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(data)
+    // }).then(res => res.json()).then(json => {
+    //   if (json.success === true) { 
+    //     console.log(json)
+    //     // this tells the child form to show success dimmer
+    //     setSuccess(true)
+    //   }
+    // }).catch(err => {
+    //   console.log(err);
+    //   return 'There was an error. See console for details'
+    // }) 
+
+    //
   }
+
   
   return (
     <div>
       <WeekInReviewForm 
         inputs={inputs} 
         onSubmitCallback={onSubmitCallback} 
+        success={success}
+        color='violet'
       />
     </div>
   )
@@ -35,41 +59,30 @@ const formStyle = {
   padding: '1.5em'
 }
 
-// props: inputs, init state, api route to hit, color, stamp-with-date true/false
 const WeekInReviewForm = (props) => {
 
-  // const { inputs, onSubmitCallback } = props
-    // form input props
-    const inputs = [
-      { name: 'weekNumber', label: 'Week #', type:'number' },
-      { name: 'focus', label: 'Focus this week', type:'text'},
-      { name: 'experienced', label: `This week I experienced`, type: 'textarea' },
-      { name: 'learned', label: `This week I learned`, type: 'textarea' }
-    ]
-
-  // const formRef = createRef()
-  // const [formReset, setFormReset] = useState(0)
-  const [formKey, setFormKey] = useState(0)
+  const inputs = props.inputs
+  const success = props.success
+  const onSubmitCallback = props.onSubmitCallback
+  const color = props.color || 'grey'
+  
   const [vals, setVals] = useState({})
   const [dimmerActive, setDimmerActive] = useState(false)
 
-  const setInitialState = (arr) => {
-    // arr.forEach(e => setVals({ ...vals, [e.name]: '' }))
-    setVals({
-      experienced: "",
-      focus: "",
-      learned: "",
-      weekNumber: ""
-    })
+  const setInitialState = () => {
+    const initState = {}
+    inputs.forEach(e => initState[e.name] = '')
+    setVals(initState)
   }
 
   useEffect(() => {
     setInitialState(inputs)
-  }, [])
+  }, [inputs])
 
   useEffect(() => {
-    console.log('current state', vals)
-  }, [vals])
+    setDimmerActive(success)
+    setInitialState(inputs)
+  }, [success])
 
   const handleChange = e => {
     setVals({ ...vals, [e.target.name]: e.target.value })
@@ -79,48 +92,19 @@ const WeekInReviewForm = (props) => {
     event.preventDefault()
     const data = {}
     // Grab the vals from event.target and populate the data object
-    // that we will send to the server
     inputs.forEach(e => data[e.name] = event.target[e.name]?.value)
-    console.log(data)
-
-    setDimmerActive(true)
-    setInitialState({...inputs, reset: true})
-
-    // reset form vals
-    // formRef.reset()
-    console.log('state after submit', vals)
-
-    // reset state
-    // inputs.forEach(e => data[e.name] = event.target[e.name]?.value)
-    
-
-
-    // fetch('api/week/create', {
-    //   method: 'POST', 
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(data)
-    // }).then(res => res.json()).then(json => {
-    //   if (json.success === true) { 
-    //     console.log(json)
-    //     // TODO: Show success message & reset state
-    //     // this.handleShow() 
-    //     // this.resetState();
-    //   }
-    // }).catch(err => {
-    //   console.log(err);
-    //   return 'There was an error. See console for details'
-    // }) 
+    // send submitted data to parent component
+    onSubmitCallback(data)
   }
 
   return (
     <>
+    {(vals && Object.keys(vals).length > 0) &&
     <Dimmable dimmed={dimmerActive} style={{borderRadius: '5px'}}>
 
       <Segment as='section'>
-        <Form onSubmit={handleSubmit} style={formStyle} key={formKey}>
-          <Header as='h2' color='violet' content='Week in Review' />
+        <Form onSubmit={handleSubmit} style={formStyle}>
+          <Header as='h2' color={color} content='Week in Review' />
           {inputs.map(e => (
             <Field key={e.name}>
               <Input 
@@ -129,16 +113,15 @@ const WeekInReviewForm = (props) => {
                 required={e.name === 'quote' ? true : false}
                 value={vals[e.name]}
                 onChange={(e) => handleChange(e)}
-                label={<Label basic pointing='right' color='violet'>
+                label={<Label basic pointing='right' color={color}>
                   {e.label}</Label>}
                 />
             </Field>
           ))}
-          <Button type='submit' color='violet'>Submit</Button>
+          <Button type='submit' color={color}>Submit</Button>
         </Form>
       </Segment>
 
-      {/* dimmer  */}
       <Dimmer active={dimmerActive} 
         onClickOutside={() => setDimmerActive(false)}>
         <Header as='h2' icon inverted>
@@ -147,12 +130,15 @@ const WeekInReviewForm = (props) => {
           <Subheader>Make sure to get some rest today!</Subheader>
         </Header>
       </Dimmer>
-    </Dimmable>
+    </Dimmable>}
     </>
   )
 }
 
-// WeekInReviewForm.propTypes = {
-
-// }
+WeekInReviewForm.propTypes = {
+  inputs: PropTypes.array.isRequired,
+  onSubmitCallback: PropTypes.func.isRequired,
+  success: PropTypes.bool,
+  color: PropTypes.string
+}
 
