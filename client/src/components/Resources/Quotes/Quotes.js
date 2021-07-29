e373275f73d4cb8eecc4219ad75efa9887d5b5c9
wrunion <winterrunion@gmail.com>
2021-07-28 20:52:06 -0700

    Add quote display page.

--- client/src/components/Resources/Quotes/Quotes.js
+++ client/src/components/Resources/Quotes/Quotes.js
@@ -1,15 +1,73 @@
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
+import { Container, Card } from 'semantic-ui-react'
 
-function Quotes(props) {
-  return (
-    <div>
-      Quotes (in progress)
-    </div>
-  )
+const colors = [
+  'orange',
+  'yellow',
+  'green',
+  'teal',
+  'blue',
+  'violet',
+  'purple',
+  'pink',
+  'grey',
+  'black'
+]
+
+const cardStyle = {
+  padding: '1.25em', 
+  overflowWrap: 'anywhere' 
 }
 
-Quotes.propTypes = {
+const containerStyle = {
+  paddingTop: '1em',
+  paddingBottom: '1em'
+}
 
+const Quotes = () => {
+
+  const [quoteData, setQuoteData] = useState([])
+
+  useEffect(() => {
+    fetch('dbt/quote/all', {
+      method: 'GET', 
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json()).then(json => {
+      if (json.success === true) { 
+        setQuoteData(json.data)
+      }
+    }).catch(err => {
+      console.log(err);
+      return 'There was an error. See console for details'
+    })  
+  }, [])
+
+  return (
+    <>
+    <Container style={containerStyle}>
+
+      {(quoteData && quoteData.length) > 0 && 
+        quoteData.map((e, index) => {
+        const { date, id, link, quote, source } = e
+        const color = colors[index%9]
+
+        return (
+          <Card fluid 
+            color={color} 
+            key={id}
+            style={cardStyle}
+            header={quote}  
+            meta={link ? 
+              <a href={link}>{source}</a> :
+              source}
+          />
+        )
+      })}
+    </Container>
+    </>
+  )
 }
 
 export default Quotes
--- routes/dbt/getQuoteByDate.js
+++ routes/dbt/getQuoteByDate.js
@@ -5,15 +5,39 @@ const moment = require('moment')
  * Route to get quote by date
 */
 
-const queryString = `SELECT quote, source, focus, link FROM quote WHERE DATE(date) = $1;`
 
 module.exports = router => {
-
+  /* Get quotes from current date */
   router.get('/quote', asyncHandler(async(req, res) => {
     try {
-
+      const queryString = `SELECT quote, source, focus, link FROM quote WHERE DATE(date) = $1;`
       const formattedDate = moment().format('YYYY, MM DD');
       const response = await db.query(queryString, [formattedDate])
+
+      if (response.rows) {
+        res.json({ 
+          success: true,
+          data: response.rows
+        })
+      } else {
+        return res.json({
+          data: null,
+          message: 'No quote found for that date'
+        })
+      }
+    } catch (err) {
+      return res.json({ 
+        data: null,
+        error: err.message
+      })
+    }
+  }))
+
+  /* Get all quotes */
+  router.get('/quote/all', asyncHandler(async(req, res) => {
+    try {
+      const queryString = `SELECT * FROM quote;`
+      const response = await db.query(queryString)
 
       if (response.rows) {
         res.json({ 
